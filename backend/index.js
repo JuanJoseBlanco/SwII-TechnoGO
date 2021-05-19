7a9722a208e015276c153da0cf529d06ff2c8659
Juan José Blanco <juan.blanco03@uptc.edu.co>
2021-05-19 15:41:42 -0500

    Cart functionality working with localStorage

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,27 @@
-import express from 'express'
-import data from './data.js'
+import express from "express";
+import data from "./data.js";
 
-const app = express()
+const app = express();
 
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-})
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
 
-app.get('/', (req, res) =>{
-    res.send('Servidor corriendo')
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not Found" });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Servidor corriendo");
+});
 
 const port = process.env.port || 5000;
 
-app.listen(port, ()=>{
-    console.log(`Serve at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
